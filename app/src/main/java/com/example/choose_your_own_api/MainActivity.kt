3bb847c8ade770e6ab06038f7c46353f3f634c85
PurpleFlower436 <bhargvih12@gmail.com>
2023-10-10 13:02:32 -0500

    changed app layout. now we have pictures of kittens!

--- app/src/main/java/com/example/choose_your_own_api/MainActivity.kt
+++ app/src/main/java/com/example/choose_your_own_api/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.choose_your_own_api
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.codepath.asynchttpclient.AsyncHttpClient
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+
+
+    private fun getMarvelCharacterInfo() {
+        val client = AsyncHttpClient()
     }
 }
