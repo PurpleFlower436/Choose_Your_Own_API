7bf94011b618dd015612a4ba3c7992763139135c
PurpleFlower436 <bhargvih12@gmail.com>
2023-10-12 14:01:59 -0500

    added Glide library and code to load a random picture of a rick and morty character

--- app/src/main/java/com/example/choose_your_own_api/MainActivity.kt
+++ app/src/main/java/com/example/choose_your_own_api/MainActivity.kt
@@ -3,24 +3,64 @@ package com.example.choose_your_own_api
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import android.widget.ImageView
+import com.bumptech.glide.Glide
 import com.codepath.asynchttpclient.AsyncHttpClient
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
 import okhttp3.Headers
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
+
+    var Rickandmorty_pic_url = ""
+
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val Rickandmorty_pic_url = getRick_and_morty_Pic()
+        Log.d("Rickandmorty_pic_url", "rick and morty pic url is set")
+        val button: Button = findViewById<Button>(R.id.get_random_rick_and_morty_pic)
+        val imageView = findViewById<ImageView>(R.id.rick_and_morty_pic)
+
     }
 
 
 
-    private fun getKittyPic() {
+    private fun getNextImage(button: Button, imageView: ImageView) {
+        button.setOnClickListener {
+            getRick_and_morty_Pic()
+
+            Glide.with(this)
+                .load(Rickandmorty_pic_url)
+                .fitCenter()
+                .into(imageView)
+        }
+    }
+
+
+    private fun getRick_and_morty_Pic() {
+        var randomInt = Random.nextInt()
         val client = AsyncHttpClient()
-        client["http://placekitten.com/200/300", object : JsonHttpResponseHandler() {
+        val characterJson = "https://rickandmortyapi.com/api/character/" + randomInt
+        client[characterJson, object : JsonHttpResponseHandler() {
             override fun onSuccess(statusCode: Int, headers: Headers, json: JsonHttpResponseHandler.JSON) {
-                Log.d("Kitty", "response successful$json")
+                Log.d("Rick and morty", "response successful$json")
+
+                Rickandmorty_pic_url = json.jsonObject.getString("image")
+
             }
+
+
+
+
+
+
 
             override fun onFailure(
                 statusCode: Int,
@@ -28,7 +68,7 @@ class MainActivity : AppCompatActivity() {
                 errorResponse: String,
                 throwable: Throwable?
             ) {
-                Log.d("Kitten Error", errorResponse)
+                Log.d("Oh no! No rick and morty picture", errorResponse)
             }
         }]
 
